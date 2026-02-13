4f32471294c8818453bcfc999dd91488910bbed5
Alexandr Morozov <morozov1982@gmail.com>
2026-02-13 20:52:59 +0500

    lesson_22: PostgreSQL

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'samplesite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         # 'ATOMIC_REQUEST': False,
+#         # 'AUTOCOMMIT': True,
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ATOMIC_REQUEST': False,
-        # 'AUTOCOMMIT': True,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5433",
     }
 }
 
